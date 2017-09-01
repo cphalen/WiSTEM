f7a74d36a21cd9787227bc9146eb22337afb12db
Ben <benjcifu@gmail.com>
2017-09-01 17:51:09 -0500

    Created file to house functions to create a forum post and comment

---
+++ forum.js
@@ -0,0 +1,12 @@
+//I don't fully know what I'm doing with Redis and databases ¯\_(ツ)_/¯
+
+var redis = require("redis");
+
+exports.createPost = function(user, text){
+
+};
+
+exports.createReply = function(user, text, postID){
+
+};
+
