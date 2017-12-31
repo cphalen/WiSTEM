7c5a7e655fce667ea1d41a4f3c3e5c9af560518b
Campbell Phalen <cphalen@ucls.uchicago.edu>
2017-12-31 17:21:50 -0600

    Change to backup Python script that saves from Redis server before backlog

--- backup.py
+++ backup.py
@@ -1,6 +1,7 @@
 import time
 import shutil
 import os
+import redis
 THREE_DAY_WAIT_TIME = 259200
 
 def copy():
@@ -9,8 +10,13 @@ def copy():
 
     shutil.copy("./dump.rdb", "./backlog/" + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + ".rdb")
 
+def save():
+    db = redis.Redis("localhost")
+    db.save()
+
 while True:
     try:
+        save()
         copy()
         time.sleep(THREE_DAY_WAIT_TIME)
     except:
